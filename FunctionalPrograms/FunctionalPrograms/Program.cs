6071557c45ff047cdf59bd1d841f5f477aa9d303
Kiran <kiranpuri532@gmail.com>
2023-07-11 14:20:15 +0530

    [Kiran]Add:\ flip coin

---
+++ FunctionalPrograms/FunctionalPrograms/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1- Flip Coin");
+                Console.WriteLine("Choose above option");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Invalide option");
+                        break;
+                }
+            }
+        }
+    }
+}
